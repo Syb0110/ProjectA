c2769d46e346fcd4d39045f39a331940309d11dd
孙亚斌 <csun058@gmail.com>
2025-07-09 16:09:31 +0800

    Update Hello.java

--- src/main/java/test1/Hello.java
+++ src/main/java/test1/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
         int a = 1;
         int c = 3;
         int b = 2;
-        int d = 10;
+        int d = 15;
         int e = 6;
         System.out.printf("test1");
     }
